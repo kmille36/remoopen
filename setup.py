ce78f495d49d517fb54ae82963e731716979ab4a
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2024-02-03 20:43:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name = "remocolab.py",
+    name = "remoopen.py",
     version = "0.1",
-    py_modules = ['remocolab'],
-    url = "https://github.com/demotomohiro/remocolab",
-    author = "demotomohiro",
+    py_modules = ['remoopen'],
+    url = "https://github.com/kmille36/remoopen/edit/master/setup.py",
+    author = "kmille36",
     install_requires = ["pyngrok"]
 )
